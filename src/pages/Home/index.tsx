491ba8725344b4fcf36770bffc66199ae9754a3a
Laercio Rios <contact@laerciorios.com>
2023-10-21 17:54:53 -0300

    :sparkles: Add social networks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,11 +8,53 @@ import {
   ProfileInfo,
   RightSide,
   ProfileDescription,
+  SocialNetworks,
+  SocialNetworkItem,
+  SocialNetworkLink,
 } from "./styles";
 import { Header } from "../../components/Header";
+import GithubLogo from "./../../assets/github.svg?react";
+import EmailLogo from "./../../assets/gmail.svg?react";
+import HackerrankLogo from "./../../assets/hackerrank.svg?react";
+import LeetCodeLogo from "./../../assets/leetcode.svg?react";
+import LinkedinLogo from "./../../assets/linkedin.svg?react";
+import XLogo from "./../../assets/x.svg?react";
 
 export function Home() {
   const { t } = useTranslation();
+
+  const socialNetworks = [
+    {
+      name: "email",
+      url: "mailto:contact@laerciorios.com",
+      icon: <EmailLogo />,
+    },
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/laerciorios/",
+      icon: <LinkedinLogo />,
+    },
+    {
+      name: "github",
+      url: "https://github.com/LaercioSR",
+      icon: <GithubLogo />,
+    },
+    {
+      name: "x",
+      url: "https://twitter.com/devlaercio",
+      icon: <XLogo />,
+    },
+    {
+      name: "hackerrank",
+      url: "https://www.hackerrank.com/profile/LaercioRios",
+      icon: <HackerrankLogo />,
+    },
+    {
+      name: "leetcode",
+      url: "https://leetcode.com/LaercioRios/",
+      icon: <LeetCodeLogo />,
+    },
+  ];
 
   return (
     <Container>
@@ -24,6 +66,17 @@ export function Home() {
             <ProfileProfession>{t("profession")}</ProfileProfession>
             <ProfileDescription>{t("description")}</ProfileDescription>
           </ProfileInfo>
+          <SocialNetworks>
+            {socialNetworks.map((socialNetwork) => {
+              return (
+                <SocialNetworkItem key={socialNetwork.name}>
+                  <SocialNetworkLink href={socialNetwork.url} target="__blank">
+                    {socialNetwork.icon}
+                  </SocialNetworkLink>
+                </SocialNetworkItem>
+              );
+            })}
+          </SocialNetworks>
         </LeftSide>
         <RightSide>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil illo
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -15,6 +15,10 @@ export const Content = styled("div")`
 `;
 
 export const LeftSide = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
   width: 400px;
   height: calc(100vh - 6rem);
 `;
@@ -41,4 +45,36 @@ export const ProfileProfession = styled("h2")`
 export const ProfileDescription = styled("p")`
   color: ${(props) => props.theme.text};
   opacity: 0.5;
+`;
+
+export const SocialNetworks = styled("ul")`
+  list-style-type: none;
+  display: flex;
+  gap: 0.25rem;
+  margin-bottom: 2rem;
+`;
+
+export const SocialNetworkItem = styled("li")`
+  width: 3rem;
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: ${(props) => props.theme.buttonHeader};
+
+    svg {
+      opacity: 0.9;
+    }
+  }
+`;
+
+export const SocialNetworkLink = styled("a")`
+  svg {
+    width: 2rem;
+    fill: ${(props) => props.theme.text};
+    opacity: 0.7;
+    transition: opacity 0.2s ease-in-out;
+  }
 `;
