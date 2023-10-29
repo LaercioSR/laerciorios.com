e71c1f2f4092700fae47501cb44674fdf4c25577
Laercio Rios <contact@laerciorios.com>
2023-10-29 13:42:15 -0300

    :sparkles: Add table for projects in large screen

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -7,10 +7,17 @@ import {
   ProjectInfo,
   ProjectInfos,
   ProjectLink,
+  Table,
+  TableBody,
+  TableBodyCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
 } from "./styles";
 import { NestedList } from "../../components/NestedList";
 import project from "../../data/projects.json";
 import { SkillsList } from "../../components/SkillsList";
+import ArrowOutward from "./../../assets/arrow-outward.svg?react";
 
 export function Projects() {
   const { t } = useTranslation();
@@ -30,7 +37,7 @@ export function Projects() {
             )}
           </ProjectInfo>
           <ProjectInfo>
-            <h4>{t("year")}: </h4>
+            <h4>{t("projects.columns.year")}: </h4>
             <p>{item.year}</p>
           </ProjectInfo>
           <ProjectInfo>
@@ -48,10 +55,59 @@ export function Projects() {
   return (
     <Container>
       <Content>
-        <h1>{t("projects.title")}</h1>
+        <h1>{t("projects.titlePage")}</h1>
         <ListContainer>
           <NestedList items={items} />
         </ListContainer>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHeaderCell>{t("projects.columns.year")}</TableHeaderCell>
+              <TableHeaderCell>{t("projects.columns.title")}</TableHeaderCell>
+              <TableHeaderCell>{t("projects.columns.bond")}</TableHeaderCell>
+              <TableHeaderCell>{t("projects.columns.link")}</TableHeaderCell>
+              <TableHeaderCell>
+                {t("projects.columns.description")}
+              </TableHeaderCell>
+              {/* <TableHeaderCell>{t("projects.columns.skills")}</TableHeaderCell> */}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {project.map((item) => {
+              return (
+                <TableRow key={item.key}>
+                  <TableBodyCell>
+                    <ProjectInfo>{item.year}</ProjectInfo>
+                  </TableBodyCell>
+                  <TableBodyCell width="180px" align="center">
+                    <h4>{t(`projects.list.${item.key}.title`)}</h4>
+                  </TableBodyCell>
+                  <TableBodyCell align="center">
+                    <ProjectInfo>
+                      <p>{t(`projects.type.${item.type}`)}</p>
+                      {item.bond && (
+                        <>
+                          <BulletDivisor />
+                          <p>{item.bond}</p>
+                        </>
+                      )}
+                    </ProjectInfo>
+                  </TableBodyCell>
+                  <TableBodyCell>
+                    <ProjectLink href={item.link} target="__blanket">
+                      {item.link}
+                      <ArrowOutward />
+                    </ProjectLink>
+                  </TableBodyCell>
+                  <TableBodyCell>
+                    <p>{t(`projects.list.${item.key}.description`)}</p>
+                    <SkillsList skills={item.skills} />
+                  </TableBodyCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
       </Content>
     </Container>
   );
--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -26,6 +26,10 @@ export const ListContainer = styled("div")`
   @media (min-width: 768px) {
     max-width: 650px;
   }
+
+  @media (min-width: 1024px) {
+    display: none;
+  }
 `;
 
 export const ProjectInfos = styled("div")`
@@ -38,13 +42,31 @@ export const ProjectInfo = styled("div")`
   display: flex;
   align-items: center;
   gap: 0.25rem;
+
+  @media (min-width: 1024px) {
+    justify-content: center;
+  }
 `;
 
 export const ProjectLink = styled("a")`
   text-decoration: none;
   color: ${(props) => props.theme.text};
+  display: flex;
+  gap: 0.25rem;
+  align-items: center;
+
+  svg {
+    width: 1.25rem;
+    height: 1.25rem;
+    fill: ${(props) => props.theme.text};
+    opacity: 0.7;
+    transition: opacity 0.2s ease-in-out;
+  }
 
   &:hover {
+    svg {
+      opacity: 1;
+    }
     text-decoration: underline;
   }
 `;
@@ -55,4 +77,54 @@ export const BulletDivisor = styled("span")`
   height: 0.3rem;
   background-color: ${(props) => props.theme.text};
   border-radius: 50%;
+`;
+
+export const Table = styled("table")`
+  display: none;
+  border-collapse: collapse;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme.text}CC;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.background};
+  }
+
+  @media (min-width: 1024px) {
+    display: block;
+  }
+`;
+
+export const TableRow = styled("tr")`
+  border-bottom: 1px solid ${(props) => props.theme.text};
+`;
+
+export const TableHeader = styled("thead")``;
+
+export const TableHeaderCell = styled("th")`
+  padding-bottom: 1rem;
+`;
+
+export const TableBody = styled("tbody")``;
+
+interface TableBodyCellProps {
+  width?: string;
+  align?: string;
+}
+
+export const TableBodyCell = styled("td")<TableBodyCellProps>`
+  width: ${(props) => props.width ?? "auto"};
+  padding: 1rem 0.5rem;
+  align-self: ${(props) => props.align ?? "auto"};
+
+  li {
+    margin-top: 0.5rem;
+  }
 `;
